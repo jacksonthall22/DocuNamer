cba2d6774ee665fe4f876bea7fb44f148bcafc9d
Jackson Hall <jacksonthall22@gmail.com>
2019-09-27 01:15:47 -0400

    Created welcome message

---
+++ docunamer.py
@@ -0,0 +1,19 @@
+def main():
+    welcome()
+
+def welcome():                                                                                                 
+    """Display a welcome message on program execution."""
+    print('`7MM"""Yb.                                 `7MN.   `7MF\'                                             ')
+    print('  MM    `Yb.                                 MMN.    M                                               ')
+    print('  MM     `Mb  ,pW"Wq.   ,p6"bo `7MM  `7MM    M YMb   M   ,6"Yb.  `7MMpMMMb.pMMMb.   .gP"Ya  `7Mb,od8 ')
+    print('  MM      MM 6W\'   `Wb 6M\'  OO   MM    MM    M  `MN. M  8)   MM    MM    MM    MM  ,M\'   Yb   MM\' "\' ')
+    print('  MM     ,MP 8M     M8 8M        MM    MM    M   `MM.M   ,pm9MM    MM    MM    MM  8M""""""   MM     ')
+    print('  MM    ,dP\' YA.   ,A9 YM.    ,  MM    MM    M     YMM  8M   MM    MM    MM    MM  YM.    ,   MM     ')
+    print('.JMMmmmdP\'    `Ybmd9\'   YMbmd\'   `Mbod"YML..JML.    YM  `Moo9^Yo..JMML  JMML  JMML. `Mbmmd\' .JMML.   ')
+    print()
+    print('Created by Jackson Hall')
+    print('v1.0')
+
+
+if __name__ == '__main__':
+    main()
