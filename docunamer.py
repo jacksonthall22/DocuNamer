d66f4f36b7b22f091acb2fc3232483c630843891
Jackson Hall <jacksonthall22@gmail.com>
2019-09-30 20:01:33 -0400

    Added basic Course class

--- docunamer.py
+++ docunamer.py
@@ -1,6 +1,16 @@
 def main():
     welcome()
 
+class Class:
+    def __init__(self, courseNum, section, courseName, professor):
+        self.courseNum = courseNum
+        self.section = section
+        self.courseName = courseName
+        self.professor = professor
+
+    def __str__(self):
+        return 'Course: ' + courseNum + section + ' ' + courseName + '; Professor: ' + professor 
+
 def welcome():                                                                                                 
     """Display a welcome message on program execution."""
     print('`7MM"""Yb.                                 `7MN.   `7MF\'                                             ')
